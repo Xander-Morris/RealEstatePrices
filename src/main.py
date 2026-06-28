808b7e3cec28134b937bb8f9ac06bf7bc2803f50
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-06-28 13:14:11 -0700

    feat: add histogram

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import tarfile 
 import urllib.request   
 import pandas as pd 
+import matplotlib.pyplot as plt 
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -15,13 +16,17 @@ def _fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
     urllib.request.urlretrieve(housing_url, tgz_path)
-    housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
-    housing_tgz.close()
+    with tarfile.open(tgz_path) as housing_tgz:
+        housing_tgz.extractall(path=housing_path, filter="data")
 
 def _main():
     _fetch_housing_data()
-    _load_housing_data()
+    housing = _load_housing_data()
+    print(housing.head())
+    print(housing.info())
+    print(housing.describe())
+    housing.hist(bins=50, figsize=(10,5))
+    plt.show()
 
 if __name__ == "__main__":
     _main()
